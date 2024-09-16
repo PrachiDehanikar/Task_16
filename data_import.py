b8584f36b37d7a5ff8623fe2104e2985d8939649
Prachi_Dehanikar <prachidehanikar@gmail.com>
2024-09-16 18:05:41 +0530

    create import export ex
    1) if i send one object it should give me all values of that object as list

---
+++ data_import.py
@@ -0,0 +1,11 @@
+
+from main import get_obj_values
+
+obj = {
+    'name': 'vivo',
+    'age':22
+}
+
+
+result = get_obj_values(obj)
+print(result)
---
+++ main.py
@@ -0,0 +1,5 @@
+
+
+
+def get_obj_values(obj):
+    return list(obj.values())
